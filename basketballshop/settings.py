33a48a169e40b93019e2b383ade60877a6e3752c
cyrille <cyrilleotieno7@gmail.com>
2021-09-24 10:17:38 +0300

    fixed setttings

--- basketballshop/settings.py
+++ basketballshop/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.media',
                 'django.contrib.messages.context_processors.messages',
                 'basket.context_processors.basket',
             ],
@@ -141,7 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,"static/")),
 
 MEDIA_URl = '/media/'
-MEDIA_ROOT = (os.path.join(BASE_DIR,"media/"))
+MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
