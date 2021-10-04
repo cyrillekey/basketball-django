e0b6f2fbf401253009a97170301c4e94547a4834
cyrille <cyrilleotieno7@gmail.com>
2021-10-04 21:07:20 +0300

    hello world

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
-from .views import checkout
+from .views import checkout,add
 app_name="order"
 
 urlpatterns=[
-    path("",checkout,name="checkout")
+    path("",checkout,name="checkout"),
+    path("add/",add,name="add")
+
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
