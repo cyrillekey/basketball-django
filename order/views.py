47f81e226a40d3d8a49eb1142df2c659985746cc
cyrille <cyrilleotieno7@gmail.com>
2021-10-05 12:10:38 +0300

    added different payment methods

--- order/views.py
+++ order/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from basket.basket import Basket
 from order.models import Order,OrderItem
 from account.forms import Addressform
@@ -24,6 +24,7 @@ def add(request):
     user_id=request.user.id
     baskettotal=basket.get_total_price()
     order_key=""
+    """
     if Order.objects.filter(order_key=order_key).exists():
         pass
     else:
@@ -33,3 +34,10 @@ def add(request):
             OrderItem.objects.create(order=order_id,product=item['product'],price=item['price'],quantity=item['qty'])
     response=JsonResponse({'success':'Order Created'})    
     return response    
+    """
+    if 'mpesa' in request.POST:
+        return HttpResponse("Pay with mpesa selected")
+    elif 'delivery' in request.POST:
+        return HttpResponse("Pay Mpesa on  Delivery")  
+    else:
+        return HttpResponse("None worked")      
