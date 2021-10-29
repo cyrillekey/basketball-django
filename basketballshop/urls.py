0cac9dd2ce972600205c0cda5552faf613ede0ab
cyrille <cyrilleotieno7@gmail.com>
2021-10-29 18:34:57 +0300

    added dashboard

--- basketballshop/urls.py
+++ basketballshop/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path,include
 from account import views as accountview
 from payment.views import mpesa_callback
+from dashboard.views import dashboard
 urlpatterns = [
    
     path('admin/', admin.site.urls),
@@ -15,7 +16,7 @@ urlpatterns = [
     path('basket/',include('basket.urls',namespace='basket')),
     path("",include('mainpage.urls',namespace='mainpage')),
     path("checkout/",include("order.urls",namespace="order")),
-    path("mpesacall/",mpesa_callback,name="mpesa")
-   
+    path("mpesacall/",mpesa_callback,name="mpesa"),
+    path("dashboard/",dashboard)
     
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
